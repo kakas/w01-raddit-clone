4842543e3fccbd30607b954665018828d45a8289
kakas <shrimptrain@gmail.com>
2015-09-07 00:27:13 +0800

    link CRUD completed

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,25 +2,51 @@ class LinksController < ApplicationController
 
 
   def index
-    @link = Link.new
+    @links = Link.all
   end
 
   def show
+    @link = Link.find(params[:id])
+  end
+
+  def new
+    @link = Link.new
   end
 
   def create
+    @link = Link.new(link_params)
+
+    if @link.save
+      redirect_to root_path, notice: "Adds link successful"
+    else
+      render :new
+    end
   end
 
   def edit
+    @link = Link.find(params[:id])
   end
 
   def update
+    @link = Link.find(params[:id])
+
+    if @link.update(link_params)
+      redirect_to root_path, notice: "Edits link successful"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect_to root_path, alert: "link has been deleted"
   end
 
   private
 
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
 
 end
