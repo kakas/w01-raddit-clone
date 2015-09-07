f44499412d424d53c3147a48d195ba7115811c1f
kakas <shrimptrain@gmail.com>
2015-09-07 21:45:31 +0800

    add notice_message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def notice_message
+    close_button_option = { class: "close", "type" => "button", "data-dismiss" => "alert", "aria-label" => "Close", "aria-hidden" => "true" }
+    close_button = content_tag(:button, '&times;'.html_safe, close_button_option)
+    alert_types = { notice: :info, alert: :danger }
+
+    message =
+      flash.map do |type, msg|
+        alert_content = close_button + msg
+        alert_type = alert_types[type.to_sym] || type
+        alert_class = { class: "alert alert-#{alert_type} alert-dismissible", role: "alert" }
+        content_tag(:div, alert_content, alert_class)
+      end
+
+    message.join('\n').html_safe
+
+
+  end
 end
